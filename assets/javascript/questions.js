9b322f189a9e501c8001e6c5a2f06bc9cf9f6811
Sittichai Chaikamol <nu@mcp.local>
2020-01-23 11:52:38 -0600

    update

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,39 +1,44 @@
 // questions
-let questions= [
-    {
-        question : "who is me",
-        a : "wrong",
-        b : "wrong",
-        c : "wrong",
-        D : "correct", 
-        correct : "d", 
+let questions= [{
+        title : "Inside which HTML element do we put the JavaScript?",
+        choices : [
+        "A: js",
+        "B: javascript",
+        "C: scripting",
+        "D: script"],
+        answer : "D: script", 
     },{
-        question : "who is me",
-        a : "wrong",
-        b : "correct",
-        c : "wrong",
-        d : "wrong", 
-        t : "b", 
+        title : "How do you round the number 7.25, to the nearest integer?",
+        choices : [
+        "A: round(7.25) ",
+        "B: Math.round(7.25",
+        "C: rnd(7.25)",
+        "D: Math.rnd(7.25)"],
+        answer : "B: Math.round(7.25)", 
     },{
-        question : "who is me",
-        a : "correct",
-        b : "wrong",
-        c : "wrong",
-        d : "wrong", 
-        correct : "a", 
+
+        title : "Where is the correct place to insert a JavaScript",
+        choices : [
+        "A: Both the <head> section and the <body> section",
+        "B: The <body> section",
+        "C: The <head> section",
+        "D: The <container> section"], 
+        answer : "A: Both the <head> section and the <body> section", 
     },{
-        question : "who is me",
-        a : "wrong",
-        b : "wrong",
-        c : "correct",
-        d : "wrong", 
-        correct : "c", 
+        qtitle : "Which event occurs when the user clicks on an HTML element?",
+        choices : [
+        "A: onMouseOver",
+        "B: onMouseClick",
+        "C: onClick",
+        "D: onChange",],
+        answer : "C: onClickc", 
     },{
-        question : "who is me",
-        a : "correct",
-        b : "wrong",
-        c : "wrong",
-        d : "wrong", 
-        correct : "a", 
+        title : "How do you create a function in JavaScript?",
+        choices : [
+        "A: function myFunction()",
+        "B: function = myFunction()",
+        "C: function:myFunction()",
+        "D: myFunction()",],
+        answer : "A: function myFunction()", 
     }
 ];
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,53 +1,111 @@
-// Select Elements
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
 
-var startEl = document.getElementById("start-btn");
-var choices = document.getElementById("choices");
-var a = document.getElementById("a");
-var b = document.getElementById("b");
-var c = document.getElementById("c");
-var d = document.getElementById("d");
-var counter = document.getElementById("counter");
-var timeGauge = document.getElementById("timeGauge");
-var scoreDiv = document.getElementById("myScore");
+function start() {
 
-let nextQuestion = 0
+timeLeft = 75;
+document.getElementById("timeLeft").innerHTML = timeLeft;
 
-
-startEl.addEventListener("click", startQuiz)
-
-
- function startQuiz() {
-     startEl.style.display = "none"
-     startEl.style.textAlign = "center"
-     setQuestion();
-
- };
-
- function setQuestion() {
-    var q = questions[nextQuestion];
-    choices.innerHTML="<p>"+ q.question +"</p>";
-    a.innerHTML = q.a;
-    b.innerHTML = q.b;
-    c.innerHTML = q.c;
-    d.innerHTML = q.d;
- };
-
- function checkAnswer(answer){
-    if( answer == questions[nextQuestion].correct){
-        score++;
-        answerIsCorrect();
-    }else{
-        answerIsWrong();
+timer = setInterval(function() {
+    timeLeft--;
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+    if (timeLeft <= 0) {
+        clearInterval(timer);
+        done(); 
     }
- }
- function answerIsCorrect(){
+}, 1000);
 
+next();
 }
-function answerIsWrong(){
-    
+
+function done() {
+clearInterval(timer);
+
+var quizContent = `
+<h2>All done!</h2>
+<h3>Your final score is ` + score +  `.</h3>
+Enter initial<input type="text" id="name" placeholder="Your initial here"> 
+<button onclick="submit()">Submit!</button>`;
+
+document.getElementById("quiz").innerHTML = quizContent;
 }
-function scoreRender(){
-    scoreDiv.style.display = "block";
-    const scorePerCent = Math.round(100 * score/questions.length);
-    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+
+function submit() {
+localStorage.setItem("highscore", score);
+localStorage.setItem("highscoreName",  document.getElementById('name').value);
+getScore();
+}
+
+
+function getScore() {
+var quizContent = `
+<h2>` + localStorage.getItem("highscoreName") +  `: Your score is ` + localStorage.getItem("highscore") + `</h2><br> 
+
+<button onclick="reset()">Go Back!</button><button onclick="clearScore()">Clear Highscores</button>
+
+`;
+
+document.getElementById("quiz").innerHTML = quizContent;
+}
+
+function clearScore() {
+localStorage.setItem("highscore", "");
+localStorage.setItem("highscoreName",  "");
+
+reset();
+}
+
+function reset() {
+clearInterval(timer);
+score = 0;
+currentQuestion = -1;
+timeLeft = 0;
+timer = null;
+
+document.getElementById("timeLeft").innerHTML = timeLeft;
+
+var quizContent = `
+<h1>Coding Quiz Challenge</h1>
+<h3>Welcome to JavaScript Quiz   </h3>
+<button onclick="start()">Start!</button>`;
+
+document.getElementById("quiz").innerHTML = quizContent;
+}
+
+function incorrect() {
+timeLeft -= 15; 
+
+next();
+}
+
+function correct() {
+score += 20;
+next();
+}
+
+function next() {
+currentQuestion++;
+
+if (currentQuestion > questions.length - 1) {
+    done();
+    return;
+}
+
+var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+
+for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+    var buttonCode = "<button onclick=\"[answer]\">[choices]</button>"; 
+    buttonCode = buttonCode.replace("[choices]", questions[currentQuestion].choices[buttonLoop]);
+    if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+        buttonCode = buttonCode.replace("[answer]", "correct()");
+    } else {
+        buttonCode = buttonCode.replace("[answer]", "incorrect()");
+    }
+    quizContent += buttonCode
+}
+
+
+document.getElementById("quiz").innerHTML = quizContent;
 }
